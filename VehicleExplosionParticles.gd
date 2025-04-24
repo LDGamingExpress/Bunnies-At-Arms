f0d51f7a4823f1e3c511df5013c53dc0d12847ae
LDGamingExpress <110698883+LDGamingExpress@users.noreply.github.com>
2025-04-24 12:20:45 -0400

    Small SFX Fix

--- VehicleExplosionParticles.gd
+++ VehicleExplosionParticles.gd
@@ -1,8 +1,13 @@
 extends GPUParticles2D
-
+var SFXObj = preload("res://SFXObj.tscn")
+var ExplosionSFX = preload("res://SFX/explosion_large_08.wav")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var NewAudio = SFXObj.instantiate()
+	NewAudio.position = global_position
+	NewAudio.stream = ExplosionSFX
+	get_parent().call_deferred("add_child",NewAudio)
 	emitting = true
 	$GPUParticles2D.emitting = true
 	$GPUParticles2D2.emitting = true
